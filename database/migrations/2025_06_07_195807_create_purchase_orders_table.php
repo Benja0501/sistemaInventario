832bd76b6ca118e4a8f7e95ed3e31c15c6d851ba
Benja0501 <g2333300121@unitru.edu.pe>
2025-06-08 14:27:31 -0500

    agregando atributos a la migracion de purchase

--- database/migrations/2025_06_07_195807_create_purchase_orders_table.php
+++ database/migrations/2025_06_07_195807_create_purchase_orders_table.php
@@ -13,7 +13,35 @@ return new class extends Migration
     {
         Schema::create('purchase_orders', function (Blueprint $table) {
             $table->id();
+            $table->string('order_number')->unique();
+            $table->unsignedBigInteger('created_by_user_id');
+            $table->unsignedBigInteger('supplier_id');
+            $table->dateTime('order_date')->nullable();
+            $table->date('expected_delivery_date')->nullable();
+            $table->decimal('total_amount', 12, 2)->default(0);
+            $table->enum('status', [
+                'pending',
+                'approved',
+                'rejected',
+                'sent',
+                'partial_received',
+                'completed',
+                'canceled'
+            ])->default('pending');
             $table->timestamps();
+
+            //referencias
+            $table->foreign('created_by_user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('no action');
+
+            $table->foreign('supplier_id')
+                ->references('id')
+                ->on('suppliers')
+                ->onUpdate('cascade')
+                ->onDelete('no action');
         });
     }
 
